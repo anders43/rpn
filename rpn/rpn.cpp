7eec44b38c0552656ff9d4770c1b0c20d3ec59dc
Anders Karlsson <anders43@gmail.com>
2019-01-16 09:36:20 +0100

    using std::optional for a more elegant way of returning an optional function pointer. also added code to copy new release version to c:\bin

--- rpn/rpn.cpp
+++ rpn/rpn.cpp
@@ -10,128 +10,168 @@
 #include <cmath>
 #include <optional>
 
-typedef double(*pfunc)(double);
-std::map<std::string, pfunc> functionMap;
-
-typedef void(*sfunc)(std::stack<double>& st);
-std::map<std::string, sfunc> stackFunctionMap;
-
-void initFuncMap()
+class RPNCalc
 {
-  functionMap["acos"] = acos;
-  functionMap["cos"] = cos;
-  functionMap["asin"] = asin;
-  functionMap["sin"] = sin;
-  functionMap["atan"] = atan;
-  functionMap["tan"] = tan;
-  functionMap["abs"] = fabs;
-  functionMap["sqrt"] = sqrt;
-  functionMap["trunc"] = trunc;
-}
+public:
+  typedef double(*pfunc)(double);
 
-void dup(std::stack<double>& st)
-{
-  auto op1 = st.top();
-  st.push(op1);
-}
-
-void swapStack(std::stack<double>& st)
-{
-  auto op1 = st.top();
-  st.pop();
-  auto op2 = st.top();
-  st.pop();
-  st.push(op1);
-  st.push(op2);
-}
-
-void initStackManipulationMap()
-{
-  stackFunctionMap["dup"] = dup;
-  stackFunctionMap["swap"] = swapStack;
-}
-
-bool isOperator(const std::string& str)
-{
-  return (str[0] == '+' || str[0] == '-' || str[0] == '*' || str[0] == '/');
-}
-
-std::pair<bool,pfunc> isUnaryFunction(const std::string& str)
-{
-  auto it = functionMap.find(str);
-  if (it != functionMap.end())
+  RPNCalc()
   {
-    std::pair<bool, pfunc> ret;
-    ret.first = true;
-    ret.second = it->second;
-    return std::make_pair(true, it->second);
+    initFuncMap();
   }
-  else
+  ~RPNCalc() = default;
+
+  bool isOperator(const std::string& str)
   {
-    return std::make_pair(false, nullptr);
+    return (str[0] == '+' || str[0] == '-' || str[0] == '*' || str[0] == '/');
   }
-}
+
+  //if (auto[exists, p] = rpn.isUnaryFunction(arg); exists) // if function exists, use pointer returned.
+  std::pair<bool, pfunc> isUnaryFunction2(const std::string& str)
+  {
+    auto it = functionMap_.find(str);
+    if (it != functionMap_.end())
+    {
+      std::pair<bool, pfunc> ret;
+      ret.first = true;
+      ret.second = it->second;
+      return std::make_pair(true, it->second);
+    }
+    else
+    {
+      return std::make_pair(false, nullptr);
+    }
+  }
+
+  // more elegant way to see if function exists and return function pointer if it does.
+  std::optional<pfunc> isUnaryFunction(const std::string& str)
+  {
+    auto it = functionMap_.find(str);
+    if (it != functionMap_.end())
+    {
+      std::pair<bool, pfunc> ret;
+      ret.first = true;
+      ret.second = it->second;
+      return std::optional<pfunc>(it->second);
+    }
+    else
+    {
+      return std::nullopt;
+    }
+  }
+
+  void plus()
+  {
+    auto op2 = st_.top();
+    st_.pop();
+    auto op1 = st_.top();
+    st_.pop();
+    st_.push(op1 + op2);
+  }
+
+  void minus()
+  {
+    auto op2 = st_.top();
+    st_.pop();
+    auto op1 = st_.top();
+    st_.pop();
+    st_.push(op1 - op2);
+  }
+
+  void multiplication()
+  {
+    auto op2 = st_.top();
+    st_.pop();
+    auto op1 = st_.top();
+    st_.pop();
+    st_.push(op1 * op2);
+  }
+
+  void division()
+  {
+    auto op2 = st_.top();
+    st_.pop();
+    auto op1 = st_.top();
+    st_.pop();
+    st_.push(op1 / op2);
+  }
+
+  void push(double value)
+  {
+    st_.push(value);
+  }
+
+  double pop()
+  {
+    auto op = st_.top();
+    st_.pop();
+    return op;
+  }
+
+  double top()
+  {
+    return st_.top();
+  }
+
+private:
+  void initFuncMap()
+  {
+    functionMap_["acos"] = acos;
+    functionMap_["cos"] = cos;
+    functionMap_["asin"] = asin;
+    functionMap_["sin"] = sin;
+    functionMap_["atan"] = atan;
+    functionMap_["tan"] = tan;
+    functionMap_["abs"] = fabs;
+    functionMap_["sqrt"] = sqrt;
+    functionMap_["trunc"] = trunc;
+  }
+
+  std::map<std::string, pfunc> functionMap_;
+  std::stack<double> st_;
+};
+
 
 int main(int argc,char* argv[])
 {
-  std::stack<double> st;
-  double op1, op2;
-  initFuncMap();
-  initStackManipulationMap();
-
+  RPNCalc rpn;
+  
   try
   {
     while (--argc)
     {
       ++argv;
       std::string arg(*argv);
-      if (isOperator(arg))
+      if (rpn.isOperator(arg))
       {
         switch (arg[0])
         {
         case '+':
-          op2 = st.top();
-          st.pop();
-          op1 = st.top();
-          st.pop();
-          st.push(op1 + op2);
+          rpn.plus();
           break;
         case '-':
-          op2 = st.top();
-          st.pop();
-          op1 = st.top();
-          st.pop();
-          st.push(op1 - op2);
+          rpn.minus();
           break;
         case '*':
-          op2 = st.top();
-          st.pop();
-          op1 = st.top();
-          st.pop();
-          st.push(op1 * op2);
+          rpn.multiplication();
           break;
         case '/':
-          op2 = st.top();
-          st.pop();
-          op1 = st.top();
-          st.pop();
-          st.push(op1 / op2);
+          rpn.division();
           break;
         }
       }
-      else if (auto[exists, p] = isUnaryFunction(arg); exists)
+      else if (auto p = rpn.isUnaryFunction(arg))
       {
-        op1 = st.top();
-        st.pop();
-        st.push(p(op1));
+        auto op1 = rpn.pop();
+        rpn.push(p.value()(op1));
       }
       else
       {
-        st.push(std::stod(arg));
+        rpn.push(std::stod(arg));
       }
     }
 
-    std::cout << st.top() << "\n";
+    std::cout << rpn.top() << "\n";
   }
   catch (const std::exception& ex)
   {
